8890436829b23c26019aa00cc4e9e4773087a061
John Paxton <pax@speedingplanet.com>
2021-09-12 19:58:46 -0400

    Routing updates

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Demos from '../views/Demos.vue';
+import Users from '../views/Users.vue';
+import Transactions from '../views/Transactions.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/demos',
+    name: 'Demos',
+    component: Demos,
+  },
+  {
+    path: '/users',
+    name: 'Users',
+    component: Users,
+  },
+  {
+    path: '/transactions',
+    name: 'Transactions',
+    component: Transactions,
+  },
+];
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+const router = createRouter( {
+  history: createWebHistory( process.env.BASE_URL ),
+  routes,
+} );
 
-export default router
+export default router;
