b3314f0194da804f1ed078ce699cb3670d1ad33c
John Paxton <pax@speedingplanet.com>
2021-09-13 12:32:55 -0400

    Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  setupFilesAfterEnv: [ '<rootDir>/jest-setup.js' ],
   transform: {
-    '^.+\\.vue$': 'vue-jest'
-  }
-}
+    '^.+\\.vue$': 'vue-jest',
+  },
+};
