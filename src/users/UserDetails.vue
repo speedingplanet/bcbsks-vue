e227520c6f1be15a69ab1ba9b5a87338ba43c11e
John Paxton <pax@speedingplanet.com>
2021-09-15 12:07:32 -0400

    Lab 1, part 5

---
+++ src/users/UserDetails.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <div class="card-header bg-info">
+      {{ user.displayName }}
+    </div>
+    <ul class="list-group">
+      <li class="list-group-item">
+        {{ user.email }}
+      </li>
+      <li class="list-group-item">
+        {{ user.address.street }}
+      </li>
+      <li class="list-group-item">
+        {{ user.address.city }}, {{ user.address.state }}
+      </li>
+      <li class="list-group-item">
+        Active? {{ user.active }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -4,10 +4,24 @@
       <h2>Users</h2>
     </div>
   </section>
+  <div class="row">
+    <div class="col-4 offset-4">
+      <UserDetails :user="users[1]" />
+    </div>
+  </div>
 </template>
 
 <script>
+import { users } from '@speedingplanet/rest-server';
+import UserDetails from '../users/UserDetails';
+
 export default {
+  components: { UserDetails },
+  data() {
+    return {
+      users,
+    };
+  },
 
 };
 </script>
