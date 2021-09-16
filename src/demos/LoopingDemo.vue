830bb632a55e92b0bc5aef6a8e853ea5d9aae91f
John Paxton <pax@speedingplanet.com>
2021-09-16 16:37:08 -0400

    v-form demo

--- src/demos/LoopingDemo.vue
+++ src/demos/LoopingDemo.vue
@@ -2,13 +2,54 @@
   <div class="row">
     <div class="col">
       <h3>Looping with v-for</h3>
+      <p>List of names</p>
+      <ul>
+        <li
+          v-for="foo in names"
+          :key="foo"
+        >
+          {{ foo }}
+        </li>
+      </ul>
+      <p>Person object</p>
+      <ul>
+        <li
+          v-for="(a, b) in person"
+          :key="a"
+        >
+          {{ b }} : {{ a }}
+        </li>
+      </ul>
+      <p>Array of people</p>
+      <ul>
+        <li
+          v-for="human in people"
+          :key="human.id"
+        >
+          {{ human.firstName }} {{ human.lastName }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      names: [ 'John', 'Dan', 'Tim', 'Andre', 'Andres', 'Hector' ],
+      person: {
+        firstName: 'John',
+        lastName: 'Paxton',
+        city: 'Nutley',
+        state: 'NJ',
+      },
+      people: [
+        { id: 1, firstName: 'John', lastName: 'Paxton' },
+        { id: 2, firstName: 'Andreina', lastName: 'Castillo' },
+      ],
+    };
+  },
 };
 </script>
 
