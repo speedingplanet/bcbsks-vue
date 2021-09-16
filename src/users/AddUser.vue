70d9a8ae058f0fb1dab9878df4b783a7f1ff0941
John Paxton <pax@speedingplanet.com>
2021-09-16 02:10:04 -0400

    For Lab 2

---
+++ src/users/AddUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <h3>Add User</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
