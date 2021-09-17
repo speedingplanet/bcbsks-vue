caf476eb8b5b1d2849bda0e46542c550c6c8be80
John Paxton <pax@speedingplanet.com>
2021-09-17 10:40:21 -0400

    Refinements

--- src/demos/PassingParams.vue
+++ src/demos/PassingParams.vue
@@ -6,6 +6,11 @@
   </div>
   <div class="row">
     <div class="col">
+      <p>{{ message }}</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
       <h4>Get params</h4>
       <DataGrid
         :columns="columns"
@@ -31,11 +36,13 @@ export default {
     return {
       columns,
       getResults: [],
+      oneTx: null,
+      message: '',
     };
   },
 
   mounted() {
-    const baseUrl = 'http://localhost:8000/api/zippay/v1/transactions/';
+    const baseUrl = 'http://localhost:8000/api/zippay/v1/transactions';
     const getParams = new URLSearchParams( { txDate_like: '2020-' } );
 
     fetch( `${baseUrl}?${getParams.toString()}` )
@@ -44,8 +51,32 @@ export default {
       .then( results => {
         console.log( 'results:', results );
         this.getResults = results;
+        this.oneTx = results[0];
+        this.oneTx.lastUpdated = new Date().toISOString();
+        this.oneTx.version = this.oneTx.version + 1;
+        updateTx( this.oneTx );
       } )
       .catch( error => console.error( 'Fetching error:', error ) );
+
+    function updateTx( tx ) {
+      fetch( `${baseUrl}/${tx.id}`,
+        {
+          method: 'PUT',
+          body: JSON.stringify( tx ),
+          headers: { 'Content-Type': 'application/json' },
+        } )
+        .then( response => {
+          if ( !response.ok ) {
+            console.log( 'Problems with the update:', response );
+            throw new Error( { message: 'Update problem', response } );
+          }
+          return response.json();
+        } )
+        .then( results => {
+          console.log( 'Updated tx:', results );
+        } )
+        .catch( error => console.error( 'Problems updating:', error ) );
+    }
   },
 };
 </script>
