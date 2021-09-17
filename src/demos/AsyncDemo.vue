b7cd57183c2ce2d24c9410cc6840459e25890ab7
John Paxton <pax@speedingplanet.com>
2021-09-17 10:20:34 -0400

    Updated demo

--- src/demos/AsyncDemo.vue
+++ src/demos/AsyncDemo.vue
@@ -6,6 +6,10 @@
         Payor {{ tx.payorId }} paid payee {{ tx.payeeId }}
         ${{ tx.amount }} on {{ tx.txDate }}.
       </p>
+      <p>
+        Payor {{ tx2.payorId }} paid payee {{ tx2.payeeId }}
+        ${{ tx2.amount }} on {{ tx2.txDate }}.
+      </p>
     </div>
   </div>
 </template>
@@ -23,12 +27,29 @@ export default {
   data() {
     return {
       tx: {},
+      tx2: {},
     };
   },
   mounted() {
+    // Using the DAO
     dao.findTransactionById( this.txId ).then( ( { data } ) => {
       this.tx = data;
     } );
+
+    // Using native fetch
+    const baseUrl = 'http://localhost:8000/api/zippay/v1/transactions/2';
+    fetch( baseUrl )
+      .then( response => {
+        if ( response.ok ) {
+          return response.json();
+        } else {
+        // Bad response, >=400. What should we do?
+        }
+      } )
+      .then( results => {
+        this.tx2 = results;
+      } )
+      .catch( error => console.error( 'Something went wrong: ', error ) );
   },
 };
 </script>
